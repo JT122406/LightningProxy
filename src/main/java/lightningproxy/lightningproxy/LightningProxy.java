fedfb45c61934e0bda0d4d546d097c791b53bb51
J.T. McQuigg <J.T.McQuigg12@gmail.com>
2022-02-14 00:51:46 -0500

    Learning Velocity API

--- src/main/java/lightningproxy/lightningproxy/LightningProxy.java
+++ src/main/java/lightningproxy/lightningproxy/LightningProxy.java
@@ -1,10 +1,11 @@
 package lightningproxy.lightningproxy;
 
 import com.google.inject.Inject;
-import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.player.ServerConnectedEvent;
+import com.velocitypowered.api.event.player.ServerPostConnectEvent;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.plugin.Plugin;
-import com.velocitypowered.api.proxy.Player;
 import com.velocitypowered.api.proxy.ProxyServer;
 import org.slf4j.Logger;
 
@@ -19,26 +20,19 @@ import org.slf4j.Logger;
 public class LightningProxy {
 
     @Inject
-    private Logger logger;
-
-
+    private ProxyServer proxyServer;
 
     @Subscribe
     public void onProxyInitialization(ProxyInitializeEvent event) {
-
         System.out.println("Loaded Lightning");
     }
 
-
-    @Inject
-    private ProxyServer proxyServer;
-
-
     @Subscribe
-    public void PostLoginEvent(Player player){
-        if (player.getCurrentServer().equals("Minigames")){
-            proxyServer.getCommandManager().executeAsync(player, "local");
+    public void chatsetter(ServerPostConnectEvent event){
+        if (event.getPlayer().getCurrentServer().get().getServerInfo().getName().equalsIgnoreCase("minigames")  || (event.getPlayer().getCurrentServer().get().getServerInfo().getName().equalsIgnoreCase("bedwars"))){
+            proxyServer.getCommandManager().executeImmediatelyAsync(event.getPlayer(), "local");
+        }else {
+            proxyServer.getCommandManager().executeImmediatelyAsync(event.getPlayer(), "global");
         }
-
     }
 }
